5291420ef074e2d03ddb28bde43e0d2914494433
bouaziziameni96-dot <bouaziziameni96@gmail.com>
2025-10-20 09:19:49 +0100

    EXERCICE1 TP1.java

--- exercice2 TP1.java
+++ exercice2 TP1.java
@@ -1,19 +1,7 @@
-//Ecrire le programme Somme.java qui lit au clavier 2 réels et affiche leur somme à l’écran
 package tp;
-
-import java.util.Scanner;
-
-public class exercice2 {
-	public static void main(String[] args) {
-		Scanner a= new Scanner(System.in);
-		System.out.println("saisir un entier x");
-		int x=a.nextInt();
-		System.out.println("saisir un entier y");
-		int y=a.nextInt();
-		int somme=x+y;
-		System.out.println("la somme est"+somme);
-		
+public class EXERCICE1 {
+	public static void main(String[]args) {
+		System.out.println("Hello World !");	
 	}
-	
 
 }
