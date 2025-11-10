71c428fb8bacfce991483722813711e8ede3003a
bouaziziameni96-dot <bouaziziameni96@gmail.com>
2025-11-10 19:45:34 +0100

    Add files via upload

--- rectangle.java
+++ rectangle.java
@@ -1,18 +1,37 @@
-package tp2;
-//bech nimporti classe scanner bech na9raa mel clavier
-import java.util.Scanner;
-//declaration les attributs
+package exercice2;
 public class rectangle {
-	double longueur;
-    double largeur;
-  //declaration constructeur de la classe
-public rectangle(double longueur, double largeur ){
-	this.longueur=longueur;
-	this.largeur=largeur;	 
-}
-public double calsurface() {
-	return longueur * largeur ;
-}
-        
+	private double longueur;
+    private double largeur;
 
+    // Constructeur avec paramètres
+    public rectangle(double longueur, double largeur) {
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+
+    // Méthode pour calculer la surface
+    public double surface() {
+        return longueur * largeur;
+    }
+
+    // Méthode pour calculer le périmètre
+    public double perimetre() {
+        return 2 * (longueur + largeur);
+    }
+
+    // Méthode pour afficher les résultats
+    public void afficher() {
+        System.out.println("Longueur : " + longueur);
+        System.out.println("Largeur : " + largeur);
+        System.out.println("Surface : " + surface());
+        System.out.println("Périmètre : " + perimetre());
+    }
+
+    // Méthode main pour tester
+    public static void main(String[] args) {
+        rectangle r = new rectangle(5, 3);
+        r.afficher();
+    }
 }
+
+
